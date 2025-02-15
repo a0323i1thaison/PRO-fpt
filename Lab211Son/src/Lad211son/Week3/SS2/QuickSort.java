0092ab42a6e5dc93fc0e5d8f9d273207ec4d7bc9
a0323i1thaison <sontds170551@fpt.edu.vn>
2025-02-15 14:10:55 +0700

    sum

--- Lab211Son/src/Lad211son/Week3/SS2/QuickSort.java
+++ Lab211Son/src/Lad211son/Week3/SS2/QuickSort.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Week3.SS2;
+package Lad211son.Week3.SS2;
 
 import java.util.Arrays;
 
--- Lab211Son/src/lab211son/Week3/SS3/Test.java
+++ Lab211Son/src/lab211son/Week3/SS3/Test.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Week3.SS3;
+package Lab211son.Week3.SS3;
 
 import java.util.Scanner;
 
